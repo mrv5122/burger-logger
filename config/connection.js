0195416b5245ba0f5f18c3e021964d911125dcc9
Manuel R <mannyvazqsan1@icloud.com>
2019-12-04 10:58:12 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -14,5 +14,13 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
 // export connection for ORM
 module.exports = connection;
