e82868935d5df16db992db90a235210c8c844d0b
Manuel R <mannyvazqsan1@icloud.com>
2019-12-04 10:46:51 -0500

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,18 @@
-// establish MySQL connection
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "us-cdbr-iron-east-05.cleardb.net",
+let connection;
+// Sets up db to connect locally or on JAWSDB if deployed
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
     port: 3306,
-    user: "b92841c4b2cc28",
-    password: "1b42559f",
-    database: "heroku_0ec484725af1444"
-});
-
-// make connection
-connection.connect(function(err) {
-    if (err) {
-    console.error("error connection: " + err.stack);
-    return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+    user: 'root',
+    password: 'Omicronf15',
+    database: 'burgers_db'
+  });
+}
 
 // export connection for ORM
 module.exports = connection;
