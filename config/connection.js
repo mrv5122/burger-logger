5761af165e05fbec18b332a8d73353a299befc88
Manuel R <mannyvazqsan1@icloud.com>
2019-12-03 23:40:43 -0500

    heroku db credentials

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "us-cdbr-iron-east-05.cleardb.net",
     port: 3306,
     user: "b92841c4b2cc28",
     password: "1b42559f",
-    database: "burgers_db"
+    database: "heroku_0ec484725af1444"
 });
 
 // make connection
